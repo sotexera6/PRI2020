ce69e03ee55a9c3988f581337adb8ba4ebf16288
so.texera@hotmail.com <so.texera@hotmail.com>
2021-01-18 13:07:41 +0000

    teste git 2

--- Teste/app-server/routes/index.js
+++ Teste/app-server/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get('http://localhost:3000/alunos')
+    .then(dados => res.render('index', {alunos: dados.data}))
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.get('/aluno/:id', function(req, res, next) {
+  axios.get('http://localhost:3000/alunos/'+ req.params.id)
+    .then(a => res.render('aluno', {aluno: a.data}))
+    .catch(e => res.render('error', {error: e}))
+});
+
+router.get('/curso/:id', function(req, res, next) {
+  axios.get('http://localhost:3000/cursos/' + req.params.id)
+    .then(dados => res.render('curso', {curso: dados.data, aluno: req.query.aluno}))
+    .catch(e => res.render('error', {error: e}))
 });
 
 module.exports = router;
